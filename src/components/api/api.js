16f8deb62e421ba8028625b6bbbdc0c5e1b6b913
Daniel Ibacache <dibacach@gmail.com>
2025-03-21 14:38:38 -0300

    se actualiza url de la api

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,43 +1,56 @@
 export const getVideos = async () => {
-  const response = await fetch("http://localhost:3000/videos");
+  const response = await fetch(
+    "https://my-json-server.typicode.com/dibacach/aluraflix/videos"
+  );
   const data = await response.json();
   return data;
 };
 
 export const getCategorias = async () => {
-  const response = await fetch("http://localhost:3000/categorias");
+  const response = await fetch(
+    "https://my-json-server.typicode.com/dibacach/aluraflix/categorias"
+  );
   const data = await response.json();
   return data;
 };
 
 export const createVideo = async (video) => {
-  const response = await fetch("http://localhost:3000/videos", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(video),
-  });
+  const response = await fetch(
+    "https://my-json-server.typicode.com/dibacach/aluraflix/videos",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(video),
+    }
+  );
   const data = await response.json();
   return data;
 };
 
 export const updateVideo = async (video) => {
-  const response = await fetch(`http://localhost:3000/videos/${video.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(video),
-  });
+  const response = await fetch(
+    `https://my-json-server.typicode.com/dibacach/aluraflix/videos/${video.id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(video),
+    }
+  );
   const data = await response.json();
   return data;
 };
 
 export const deleteVideo = async (id) => {
-  const response = await fetch(`http://localhost:3000/videos/${id}`, {
-    method: "DELETE",
-  });
+  const response = await fetch(
+    `https://my-json-server.typicode.com/dibacach/aluraflix/videos/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
   const data = await response.json();
   return data;
 };
